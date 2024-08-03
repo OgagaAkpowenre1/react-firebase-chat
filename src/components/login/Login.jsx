a28ac53195af376614280202188b213a74cbeea9
OgagaAkpowenre1 Daniel <ogagaogheneakpowenre@gmail.com>
2024-08-03 20:44:13 +0000

    On branch starter  Changes to be committed: 	modified:   package-lock.json 	modified:   package.json 	modified:   src/App.jsx 	modified:   src/components/login/Login.jsx

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -2,7 +2,10 @@ import React from "react";
 import { useState } from "react";
 import "./login.css";
 import { toast } from "react-toastify";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 import { auth, db } from "../../lib/firebase";
 import { doc, setDoc } from "firebase/firestore";
 import upload from "../../lib/uploads";
@@ -13,7 +16,7 @@ const Login = () => {
     url: "",
   });
 
-  const [loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(false);
 
   const handleAvatar = (e) => {
     if (e.target.files[0]) {
@@ -25,45 +28,55 @@ const Login = () => {
   };
 
   const handleRegister = async (e) => {
-    e.preventDefault()
-    setLoading(true)
-    
-    const formData = new FormData(e.target)
+    e.preventDefault();
+    setLoading(true);
 
-    const {username, email, password} = Object.fromEntries(formData)
+    const formData = new FormData(e.target);
+
+    const { username, email, password } = Object.fromEntries(formData);
 
     try {
-
       const res = await createUserWithEmailAndPassword(auth, email, password);
 
-      const imgUrl = await upload(avatar.file)
+      const imgUrl = await upload(avatar.file);
 
       await setDoc(doc(db, "users", res.user.uid), {
         username,
-        email, 
-        avatar : imgUrl,
-        id : res.user.uid,
-        blocked : [],
+        email,
+        avatar: imgUrl,
+        id: res.user.uid,
+        blocked: [],
       });
 
       await setDoc(doc(db, "userchats", res.user.uid), {
-        chats : [],
+        chats: [],
       });
 
-      toast.success("Account created!")
-      
+      toast.success("Account created!");
     } catch (error) {
       console.log(error);
-      toast.error(error.message)
+      toast.error(error.message);
     } finally {
-      setLoading(false)
+      setLoading(false);
     }
-  }
+  };
 
-  const handleLogin = (e) => {
-    e.preventDefault()
-    toast.error("")
-  }
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    const formData = new FormData(e.target);
+    const { email, password } = Object.fromEntries(formData);
+
+    try {
+      await signInWithEmailAndPassword(auth, email, password)
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
     <div className="login">
@@ -72,7 +85,9 @@ const Login = () => {
         <form onSubmit={handleLogin}>
           <input type="email" placeholder="Email" name="email" />
           <input type="password" placeholder="Password" name="password" id="" />
-          <button disabled={loading}>{loading ? "Loading..." : "Sign In"}</button>
+          <button disabled={loading}>
+            {loading ? "Loading..." : "Sign In"}
+          </button>
         </form>
       </div>
       <div className="separator"></div>
@@ -80,7 +95,7 @@ const Login = () => {
         <h2>Create an Account</h2>
         <form onSubmit={handleRegister}>
           <label htmlFor="file">
-            <img src={avatar.url || './avatar.png'} alt="" />
+            <img src={avatar.url || "./avatar.png"} alt="" />
             Upload an Image
           </label>
           <input
@@ -93,7 +108,9 @@ const Login = () => {
           <input type="text" placeholder="Username" name="username" />
           <input type="email" placeholder="Email" name="email" />
           <input type="password" placeholder="Password" name="password" id="" />
-          <button disabled={loading}>{loading ? "Loading..." : "Sign Up"}</button>
+          <button disabled={loading}>
+            {loading ? "Loading..." : "Sign Up"}
+          </button>
         </form>
       </div>
     </div>
